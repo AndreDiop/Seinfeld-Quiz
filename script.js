7eb08efacea5d168e8bc32d6d388524b877feff7
Andre Diop <itsjustandre@gmail.com>
2020-12-14 17:22:18 -0500

    added hide function onto starat button, barebones JS included

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
-var questions = [
-    { q: "The sky is blue.", a: "t" },
-    { q: "There are 365 days in a year.", a: "t" },
-    { q: "There are 42 ounces in a pound.", a: "f" },
-    { q: "The Declaration of Independence was created in 1745.", a: "f" },
-    { q: "Bananas are vegetables.", a: "f" }
-  ];
+// var questions = [
+//     { q: "Inside which HTML element do we put the JavaScript?", a: "<script>" },
+//     { q: " What is the correct syntax for referring to an external script called "xxx.js"?", a: "script.js" },
+//     { q: "How do you call a function named "myFunction"?", a: "myfunction()" },
 
-  console.log(questions);
+//   ];
+//   var quizTimer = 60;
+
+//   // Loop over every question object
+//   for (var i = 0; i < questions.length; i++) {
+//     // Display current question to user and ask OK/Cancel
+//     var answer = confirm(questions[i].q);
+
+//     // Compare answers
+//     if ((answer === true && questions[i].a === "t") ||
+//       (answer === false && questions[i].a === "f")) {
+//       // Increase score
+//       score++;
+//       alert("Correct!");
+//     }
+//     else {
+//       alert("Wrong!");
+//     }
+//   }
+
+//   // Show total at end
+//   alert("You got " + score + "/" + questions.length);
+
+//   var divSwitcher = document.querySelector("#div-switcher");
+//   var container = document.querySelector(".container");
+//   var mode = "dark";
+
+//   divSwitcher.addEventListener("click", function() {
+//     if (mode === "dark") {
+//       mode = "light";
+//       container.setAttribute("class", "light");
+//     }
+//     else {
+//       mode = "dark";
+//       container.setAttribute("class", "dark");
+//     }
+//   });
+
+var landingPage = document.getElementById("landing");
+var quizPage = document.getElementById("quiz");
+var timerPage = document.querySelector("#timer container");
+var scorePage = document.querySelector("score-container");
+var startButton = document.getElementById("startButton");
+
+startButton.addEventListener("click", function () {
+  // console.log("Clicked");
+  landingPage.setAttribute("class", "hide");
+  quizPage.setAttribute("class", "");
+});
