bdd0a41e3970c4ce5e5f48de7ddb923e9ed3003c
Andre Diop <itsjustandre@gmail.com>
2020-12-14 22:09:15 -0500

    added q/a and for loop

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var landingPage = document.getElementById("landing");
 var quizPage = document.getElementById("quiz");
 var timerPage = document.querySelector("#timer container");
@@ -10,3 +9,57 @@ startButton.addEventListener("click", function () {
   landingPage.setAttribute("class", "hide");
   quizPage.setAttribute("class", "");
 });
+
+var questionsIndex = [
+  {
+    question: "Commonly used data types DO NOT include...",
+    answerChoices: ["a. Strings", "b. Boolean", "c. Alerts", "d. Numbers"],
+    correctAnswer: 2,
+  },
+  {
+    question: "A condition in an If/Else statement is enclosed in...",
+    answerChoices: [
+      "a. Quotations');",
+      "b. Curly Braces",
+      "c. Parenthesis",
+      "d. Square Brackets",
+    ],
+    correctAnswer: 2,
+  },
+  {
+    question: "Arrays in Javascript can be used to store...",
+    answerChoices: [
+      "a. Numbers and Strings",
+      "b. Other Arrays",
+      "c. Booleans",
+      "d. All of the above",
+    ],
+    correctAnswer: 0,
+  },
+  {
+    question:
+      "A useful tool used during development and debugging for printing content to the debugger is...",
+    answerChoices: [
+      "a. Javascript",
+      "b. Terminal",
+      "c. For Loops",
+      "d. Console.log",
+    ],
+    correctAnswer: 3,
+  },
+  {
+    question:
+      "String values must be enclosed within ________ when being assigned to variables? ",
+    answerChoices: [
+      "a. Commas",
+      "b. Curly Braces",
+      "c. Quotations",
+      "d. Parenthesis ",
+    ],
+    correctAnswer: 2,
+  },
+];
+
+for (var i = 0; i < questionsIndex.length; i++) {
+  console.log(questionsIndex);
+}
